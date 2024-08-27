19c596c2b20cc9d9d17b92dc53c987ff9286df44
HyanCat <hyancat.cn@gmail.com>
2024-08-27 20:10:32 +0800

    Update SwiftLMDB.

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-        .package(url: "https://github.com/agisboye/SwiftLMDB.git", from: "2.1.0"),
+        .package(url: "https://github.com/HyanCat/SwiftLMDB.git", branch: "develop"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
